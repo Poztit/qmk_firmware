e6ec4f7324020eebf9ba0d7642299ce0c3fdc4d0
François Illien <francois@illien.org>
2022-02-20 21:16:13 +0100

    Disable bongo cat and cleaning all the config

--- keyboards/lily58/keymaps/poztit/rules.mk
+++ keyboards/lily58/keymaps/poztit/rules.mk
@@ -1,31 +1,24 @@
-# Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
-#
 BOOTLOADER = atmel-dfu
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-MIDI_ENABLE = no            # MIDI controls
-AUDIO_ENABLE = no           # Audio output on port C6
-UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
-SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_DRIVER_ENABLE= yes     # OLED display
+
+AUDIO_ENABLE       = no   # Audio output on port C6
+BACKLIGHT_ENABLE   = no   # Enable keyboard backlight functionality
+BLUETOOTH_ENABLE   = no   # Enable Bluetooth with the Adafruit EZ-Key HID
+BOOTMAGIC_ENABLE   = no   # Virtual DIP switch configuration
+COMMAND_ENABLE     = no   # Commands for debug and configuration
+CONSOLE_ENABLE     = no   # Console for debug
+MIDI_ENABLE        = no   # MIDI controls
+MOUSEKEY_ENABLE    = no   # Mouse keys
+RGBLIGHT_ENABLE    = no   # Enable WS2812 RGB underlight.
+SWAP_HANDS_ENABLE  = no   # Enable one-hand typing
+UNICODE_ENABLE     = no   # Unicode
+
+EXTRAKEY_ENABLE    = yes  # Audio control and System control
+NKRO_ENABLE        = yes  # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+OLED_DRIVER_ENABLE = yes  # OLED display
+WPM_ENABLE         = yes
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
-# If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
-        #./lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
+#LTO_ENABLE = yes
+SRC +=  ./lib/logo_reader.c
